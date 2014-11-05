bdf47dcba8e84c6ef893800ca84c591a933d06de
alumno355ecb16 <dc99es@gmail.com>
2014-11-05 09:31:41 -0600

    2subida

--- jquery-mobile/acciones.js
+++ jquery-mobile/acciones.js
@@ -1,18 +1,18 @@
 // JavaScript Document
 
-$(document).ready(function(e) {
-    document.addEventListener("deviceready", function(){
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
 		
-		$('#izquierda').swipeleft(function(){
-			navigator.notification.alert("Deslizo a la izquierda",function(){"Practicca4","Aceptar"});
-            });//barrer izquierda
+		$('#izquierda').on ("swipeleft", function(){
+			navigator.notification.alert("Deslizó a la izquierda",function(){"Practica4","Aceptar"});
+			});//barrer izquierda
 			
 			
-			$('#derecha').swiperight(function(){
-			   navigator.notification.confirm("¿Que quieres hacer?",function(opt){
-		    switch(opt)
+			$('#derecha').on ("swiperight", function(){
+				navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+			switch(opt)
 			{
-				
+			
 			case 1:
 			navigator.notification.beep(1);
 			break;
@@ -21,10 +21,10 @@ $(document).ready(function(e) {
 			navigator.notification.vibrate(1000);
 			break;
 			}//switch opt
-			   },"Examen2","Beep,Vibrar,Camcelar");//confirm
-			   
-			   
-            });//barrer derecha
+				},"Practica4","Beep,Vibrar,Cancelar");//confirm
+				
+			});//barrer derecha
+			
 			
 	},false);//ready device
 });//document
